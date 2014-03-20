9e56d1310b2b6d3fb118ee19a589a54a67e76287
xero <x@xero.nu>
2014-03-20 11:55:20 -0400

    fix lain calendar currect day highlighting (just added the -h flag to /usr/bin/cal calls)

--- .config/awesome/lain/widgets/calendar.lua
+++ .config/awesome/lain/widgets/calendar.lua
@@ -37,13 +37,13 @@ function calendar:show(t_out, inc_offset)
     local tims = t_out or 0
     local f, c_text
     local today = tonumber(os.date('%d'))
-    local init_t = '/usr/bin/cal | sed -r -e "s/(^| )( '
+    local init_t = '/usr/bin/cal -h | sed -r -e "s/(^| )( '
 
     if offs == 0
     then -- current month showing, today highlighted
         if today >= 10
         then
-           init_t = '/usr/bin/cal | sed -r -e "s/(^| )('
+           init_t = '/usr/bin/cal -h | sed -r -e "s/(^| )('
         end
 
         calendar.offset = 0
@@ -80,7 +80,7 @@ function calendar:show(t_out, inc_offset)
 
        calendar.notify_icon = nil
 
-       f = io.popen('/usr/bin/cal ' .. month .. ' ' .. year)
+       f = io.popen('/usr/bin/cal -h ' .. month .. ' ' .. year)
     end
 
     c_text = "<tt><span font='" .. calendar.font .. " "
