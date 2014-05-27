012101269069b6aa6cde5b04c16423b9914b3357
xero <x@xero.nu>
2014-05-27 17:41:49 -0400

    add code sauce

--- .zsh/colored-man-pages.zsh
+++ .zsh/colored-man-pages.zsh
@@ -1,3 +1,4 @@
+#█▓▒░ source: https://github.com/imkira/dotfiles/blob/master/.zsh/colored-man-pages.zsh
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;38;5;74m'
 export LESS_TERMCAP_me=$'\E[0m'
