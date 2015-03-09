2de76bc66045b997087121735e3db76e7d6f6778
andrew harrison <andrew.harrison@brandingbrand.com>
2015-03-09 11:45:35 -0400

    merge upstream zsh reorganization and update for work machine

--- zsh/.zsh/custom-prompt.zsh
+++ zsh/.zsh/custom-prompt.zsh
@@ -14,11 +14,27 @@
 # ░▓▓▓▓▓▓▓▓▓▓
 # ░░░░░░░░░░
 #
-#█▓▒░ source: https://github.com/imkira/dotfiles/blob/master/.zsh/colored-man-pages.zsh
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;38;5;74m'
-export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[38;33;246m'
-export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[04;38;5;146m'
+#█▓▒░ colors for permissions
+if [[ "$EUID" -ne "0" ]]
+then  # if user is not root
+	USER_LEVEL="%F{cyan}"
+else # root!
+	USER_LEVEL="%F{red}"
+fi
+
+#█▓▒░ allow functions in the prompt
+setopt PROMPT_SUBST
+
+#█▓▒░ autoload zsh functions
+fpath=(~/.zsh/functions $fpath)
+autoload -U ~/.zsh/functions/*(:t)
+ 
+#█▓▒░ enable auto-execution of functions
+typeset -ga preexec_functions
+typeset -ga precmd_functions
+typeset -ga chpwd_functions
+ 
+#█▓▒░ prepend git functions needed for prompt
+preexec_functions+='preexec_update_git_vars'
+precmd_functions+='precmd_update_git_vars'
+chpwd_functions+='chpwd_update_git_vars'
