48a49083115a7bd3478a0259ee1a1f52a1612ee9
xero <x@xero.nu>
2015-04-18 14:44:29 -0400

    add some nodejs aliases

--- zsh/.zsh/nodejs.zsh
+++ zsh/.zsh/nodejs.zsh
@@ -16,3 +16,9 @@
 #
 #█▓▒░ node version manager
 source ~/.nvm/nvm.sh
+
+#█▓▒░ aliases
+alias node="sudo node"
+alias npm="sudo npm"
+alias npminstall="sudo rm -rf node_modules && sudo npm cache clear && sudo npm cache clean && npm install"
+# alias npminstall="sudo rm -rf node_modules && sudo npm cache clear && sudo npm cache clean && sudo PYTHON=/usr/bin/python2 npm install"
