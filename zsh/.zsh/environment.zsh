29f030ccd3c2969c340f42c468b3b09071b67d14
xero <x@xero.nu>
2015-05-02 02:48:12 -0400

    update zsh environment vars

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -15,15 +15,24 @@
 # ░░░░░░░░░░
 #
 #█▓▒░ timestamps
-#HIST_STAMPS="mm/dd/yyyy"
+#HIST_STAMPS=mm/dd/yyyy
 
 #█▓▒░ paths
 export PATH=$HOME/bin:/usr/local/bin:/home/xero/.gem/ruby/2.2.0/bin:$PATH
-#export MANPATH="/usr/local/man:$MANPATH"
+#export MANPATH=/usr/local/man:$MANPATH
 
 #█▓▒░ preferred editor for local and remote sessions
-export EDITOR='vim'
-export VISUAL='vim'
+export EDITOR=vim
+export VISUAL=vim
 
 #█▓▒░ language
-export LC_CTYPE=en_US.utf8
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LESSCHARSET=utf-8
