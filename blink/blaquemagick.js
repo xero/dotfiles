961b4e1ab924dff43bd904e536b9bc8686891b87
xero <x@xero.style>
2023-05-10 17:24:57 -0400

    feat(blink): add blink shell style files

---
+++ blink/blaquemagick.js
@@ -0,0 +1,31 @@
+// ┏┓ ╻  ┏━┓┏━┓╻ ╻┏━╸   ┏┳┓┏━┓┏━╸╻┏━╸╻┏
+// ┣┻┓┃  ┣━┫┃┓┃┃ ┃┣╸    ┃┃┃┣━┫┃╺┓┃┃  ┣┻┓
+// ┗━┛┗━╸╹ ╹┗┻┛┗━┛┗━╸   ╹ ╹╹ ╹┗━┛╹┗━╸╹ ╹
+// blaque magick by xero harrison (https://x-e.ro)
+
+black       = '#1c1c1c';
+red         = '#5F8787';
+green       = '#666666';
+yellow      = '#87875f';
+blue        = '#8787af';
+magenta     = '#808080';
+cyan        = '#666666';
+white       = '#cccccc';
+lightBlack  = '#1c1c1c';
+lightRed    = '#5F8787';
+lightGreen  = '#a8a8a8';
+lightYellow = '#87875f';
+lightBlue   = '#8787af';
+lightMagenta= '#c2c2b0';
+lightCyan   = '#666666';
+lightWhite  = '#cccccc';
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#c2c2b0');
+t.prefs_.set('foreground-color', '#c2c2b0');
+t.prefs_.set('background-color', '#222222');
---
+++ blink/sourcerer.js
@@ -0,0 +1,39 @@
+//   ██████  ██████  ██   ██ ██████  █████   █████  ██████  █████  ██████
+//  ██░░░░  ██░░░░██░██  ░██░░██░░████░░░██ ██░░░██░░██░░████░░░██░░██░░██
+// ░░█████ ░██   ░██░██  ░██ ░██ ░░░██  ░░ ░███████ ░██ ░░░███████ ░██ ░░
+//  ░░░░░██░██   ░██░██  ░██ ░██   ░██   ██░██░░░░  ░██   ░██░░░░  ░██
+//  ██████ ░░██████ ░░██████░███   ░░█████ ░░██████░███   ░░██████░███
+// ░░░░░░   ░░░░░░   ░░░░░░ ░░░     ░░░░░   ░░░░░░ ░░░     ░░░░░░ ░░░
+//  r  e  a  d     c  o  d  e     l  i  k  e     a     w  i  z  a  r  d
+//
+// sourcerer by xero harrison (https://x-e.ro)
+//  ├─ based on sorcerer by Jeet Sukumaran (http://jeetworks.org)
+//  └─ based on mustang by Henrique C. Alves (hcarvalhoalves@gmail.com)
+
+// █▓▒░ scorcerer colors
+black       = '#000000';
+red         = '#aa4450';
+green       = '#719611';
+yellow      = '#ff9800';
+blue        = '#6688aa';
+magenta     = '#8f6f8f';
+cyan        = '#528b8b';
+white       = '#d3d3d3';
+lightBlack  = '#272822';
+lightRed    = '#b1d631';
+lightGreen  = '#b1d631';
+lightYellow = '#87875f';
+lightBlue   = '#90b0d1';
+lightMagenta= '#8181a6';
+lightCyan   = '#87ceeb';
+lightWhite  = '#c1cdc1';
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#c2c2b0');
+t.prefs_.set('foreground-color', '#c2c2b0');
+t.prefs_.set('background-color', '#222222');
