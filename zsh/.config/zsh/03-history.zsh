942b7e163efd92d9b489b6f2169abb58c4e454ba
xero <x@xero.style>
2023-06-04 12:10:44 -0400

    fix(zsh): cleanup history 🐚

--- zsh/.config/zsh/03-history.zsh
+++ zsh/.config/zsh/03-history.zsh
@@ -1,11 +1,10 @@
-#                 ██      
-#                ░██      
-#  ██████  ██████░██      
-# ░░░░██  ██░░░░ ░██████  
-#    ██  ░░█████ ░██░░░██ 
-#   ██    ░░░░░██░██  ░██ 
-#  ██████ ██████ ░██  ░██ 
-# ░░░░░░ ░░░░░░  ░░   ░░  
+#                 ██
+#  ██████  ██████░██
+# ░░░░██  ██░░░░ ░██████
+#    ██  ░░█████ ░██░░░██
+#   ██    ░░░░░██░██  ░██
+#  ██████ ██████ ░██  ░██
+# ░░░░░░ ░░░░░░  ░░   ░░
 #
 #  ▓▓▓▓▓▓▓▓▓▓
 # ░▓ author ▓ xero <x@xero.style>
@@ -15,10 +14,23 @@
 # ░░░░░░░░░░
 #
 #█▓▒░ history
-HISTFILE=~/.zhistory
-HISTSIZE=120000
+HISTSIZE=130000
 SAVEHIST=100000
+export HISTFILE="$XDG_STATE_HOME"/zsh/history
+export LESSHISTFILE=-
+
+#█▓▒░ timestamps
+#HIST_STAMPS=yyyy/mm/dd
+
+#█▓▒░ options
 setopt append_history
 setopt extended_history
 setopt hist_ignore_all_dups
 setopt share_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt hist_find_no_dups
+setopt hist_ignore_space
+setopt hist_save_no_dups
+setopt hist_reduce_blanks
