ee7316ebb86d919206ff931255a217b5ac275105
xero <x@xero.style>
2023-06-20 12:39:08 -0400

    feat(nvim): telescope overrides 🔭

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -9,18 +9,24 @@ return {
 		local key = vim.keymap.set
 		local telescope = require("telescope")
 		local lga_actions = require("telescope-live-grep-args.actions")
+		local dashed = { "┄", "┊", "┄", "┊", "╭", "╮", "╯", "╰" }
 		telescope.setup({
 			defaults = {
 				layout_strategy = "horizontal",
 				layout_config = {
 					horizontal = { width = 0.8, preview_width = 0.65, anchor = "CENTER" },
 				},
+				borderchars = {
+					prompt  = dashed,
+					results = dashed,
+					preview = dashed,
+				},
 			},
 			extensions = {
 				undo = {
 					use_delta = true,
 					side_by_side = true,
-					diff_context_lines = 6, -- vim.o.scrolloff,
+					diff_context_lines = 8, -- vim.o.scrolloff,
 					entry_format = "state #$ID, $STAT, $TIME",
 					time_format = "",
 					layout_strategy = "vertical",
@@ -40,7 +46,7 @@ return {
 					mappings = {
 						i = {
 							["<C-\\>"] = lga_actions.quote_prompt({ postfix = " --hidden " }),
-						}
+						},
 					},
 				},
 			},
