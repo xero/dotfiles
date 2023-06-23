f73026a760db21c9e3c42b61de18ecfd19ed9691
xero <x@xero.style>
2023-06-22 20:31:03 -0400

    feat(nvim): copilot explortation 💻

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -5,11 +5,12 @@ return {
 		cmd = "Copilot",
 		build = ":Copilot auth",
 		opts = {
-			suggestion = { enabled = false },
+			suggestion = { enabled = true },
 			panel = { enabled = false },
 			filetypes = {
 				markdown = true,
 				help = true,
+				lua = true,
 			},
 		},
 	},
@@ -24,8 +25,6 @@ return {
 				config = function(_, opts)
 					local copilot_cmp = require("copilot_cmp")
 					copilot_cmp.setup(opts)
-					-- attach cmp source whenever copilot attaches
-					-- fixes lazy-loading issues with the copilot cmp source
 					require("utils.functions").on_attach(function(client)
 						if client.name == "copilot" then
 							copilot_cmp._on_insert_enter({})
@@ -37,21 +36,15 @@ return {
 		---@param opts cmp.ConfigSchema
 		opts = function(_, opts)
 			local cmp = require("cmp")
-			loc
-			table.insert(opts.sources, 1, { name = "copilot", group_index = 2 })
+			table.insert(opts, { sources = { name = "copilot", group_index = 2 } })
 
 			opts.sorting = {
 				priority_weight = 2,
 				comparators = {
 					require("copilot_cmp.comparators").prioritize,
-
-					-- Below is the default comparitor list and order for nvim-cmp
 					cmp.config.compare.offset,
-					-- cmp.config.compare.scopes, --this is commented in nvim-cmp too
 					cmp.config.compare.exact,
 					cmp.config.compare.score,
-					cmp.config.compare.recently_used,
-					cmp.config.compare.locality,
 					cmp.config.compare.kind,
 					cmp.config.compare.sort_text,
 					cmp.config.compare.length,
