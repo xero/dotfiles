5b4ab69aa3a83810e7c02fbf90668a0d8d983288
xero <x@xero.style>
2023-06-30 17:02:47 -0400

    fix(zsh): completion updates 🐚

--- zsh/.config/zsh/06-aliases.zsh
+++ zsh/.config/zsh/06-aliases.zsh
@@ -39,7 +39,7 @@ alias y="yank"
 alias k="vpnns -- kubectl"
 alias kx="kubectx"
 alias k9s="vpnns -- k9s"
-alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e";echo "╙────────────────────────────────────── ─ ─ "; df -h;'
+alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e";echo "╙────────────────────────────────────── ─ ─ "; df -h;echo "╓───── s w a p s "; echo "╙────────────────────────────────────── ─ ─ "; swapon --show'
 
 #lazy
 alias "cd.."="cd ../"
