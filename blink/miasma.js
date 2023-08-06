46f338799712e193586efa4747e41cc7b46ddad7
xero <x@xero.style>
2023-08-06 01:53:05 -0400

    fix(blink-colors): яєƒα¢тσя мιαѕмα & ƒιχ ¢υяѕσя 🎨

--- blink/miasma.js
+++ blink/miasma.js
@@ -3,29 +3,22 @@
 // ┛ ┇┇┛ ┇━━┛┛ ┇┛ ┇
 //  miasma by xero (https://x-e.ro)
 
-black='#222222'
-lightBlack='#666666'
-red='#685742'
-lightRed='#685742'
-green='#5f875f'
-lightGreen='#5f875f'
-yellow='#B36D43'
-lightYellow='#B36D43'
-blue='#78824B'
-lightBlue='#78824b'
-magenta='#bb7744'
-lightMagenta='#bb7744'
-cyan='#C9A554'
-lightCyan='#C9A554'
-white='#D7C483'
-lightWhite='#D7C483'
+black = '#222222'
+altBlack = '#666666'
+red = '#685742'
+green = '#5f875f'
+yellow = '#B36D43'
+blue = '#78824B'
+magenta = '#bb7744'
+cyan = '#C9A554'
+white = '#D7C483'
 
-t.prefs_.set('color-palette-overrides',
-                 [ black , red     , green  , yellow,
-                  blue     , magenta , cyan   , white,
-                  lightBlack   , lightRed  , lightGreen , lightYellow,
-                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
-
-t.prefs_.set('cursor-color', '#918f6e');
-t.prefs_.set('foreground-color', '#c2c2b0');
+t.prefs_.set('color-palette-overrides', [
+	black, red, green, yellow,
+	blue, magenta, cyan, white,
+	altBlack, red, green, yellow,
+	blue, magenta, cyan, white
+]);
+t.prefs_.set('cursor-color', 'rgba(105, 168, 80, 0.5)');
 t.prefs_.set('background-color', '#222222');
+t.prefs_.set('foreground-color', '#D7C483');
