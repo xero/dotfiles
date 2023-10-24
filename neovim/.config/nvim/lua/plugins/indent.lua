55972eef23bc8702f7de46f643f2a47cb6c7a17a
xero <x@xero.style>
2023-10-24 16:55:26 -0400

    fix(neovim): ιη∂єηт вℓαηкℓιηє υρ∂αтєѕ 📝

--- neovim/.config/nvim/lua/plugins/indent.lua
+++ neovim/.config/nvim/lua/plugins/indent.lua
@@ -2,13 +2,18 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = "VeryLazy",
 	config = function()
-		vim.g.indent_blankline_context_char = "┊"
-		vim.g.indent_blankline_char_list = { "┊", "┊", "┊", "┊" }
-		vim.g.indent_blankline_char_list_blankline = { "┊", "┊", "┊", "┊" }
-		require("indent_blankline").setup({
-			space_char_blankline = " ",
-			show_current_context = true,
-			show_current_context_start = false,
+		require("ibl").setup({
+			scope = {
+				show_start = false,
+			},
+			indent = {
+				char = "┊",
+				tab_char = "┊",
+				smart_indent_cap = true,
+			},
+			whitespace = {
+				remove_blankline_trail = true,
+			},
 		})
 	end,
 }
