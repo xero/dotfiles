59c18743e9ae85afb827e218c4b2e83e3fdd2464
xero <x@xero.style>
2025-07-27 23:39:41 -0400

    feat(blink): adds blizzard orb colors 🔵

---
+++ blink/blizzard-orb.js
@@ -0,0 +1,31 @@
+// ┏┓ ╻  ┏━┓┏━┓╻ ╻┏━╸   ┏┳┓┏━┓┏━╸╻┏━╸╻┏
+// ┣┻┓┃  ┣━┫┃┓┃┃ ┃┣╸    ┃┃┃┣━┫┃╺┓┃┃  ┣┻┓
+// ┗━┛┗━╸╹ ╹┗┻┛┗━┛┗━╸   ╹ ╹╹ ╹┗━┛╹┗━╸╹ ╹
+// blaque magick by xero harrison (https://x-e.ro)
+
+black       = '#000000';
+red         = '#6688AA';
+green       = '#94B6CC';
+yellow      = '#4C6E84';
+blue        = '#6688AA';
+magenta     = '#94B6CC';
+cyan        = '#4C6E84';
+white       = '#87875f';
+lightBlack  = '#111111';
+lightRed    = '#6688AA';
+lightGreen  = '#94B6CC';
+lightYellow = '#4C6E84';
+lightBlue   = '#6688AA';
+lightMagenta= '#94B6CC';
+lightCyan   = '#4C6E84';
+lightWhite  = '#87875f';
+
+t.prefs_.set('color-palette-overrides',
+[ black , red     , green  , yellow,
+blue     , magenta , cyan   , white,
+lightBlack   , lightRed  , lightGreen , lightYellow,
+lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#94B6CC');
+t.prefs_.set('foreground-color', '#111111');
+t.prefs_.set('background-color', '#ffffff');
